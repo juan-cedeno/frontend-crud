e47ce575627fd3ab4e7e9027e5d938509d2e4843
Juan Cedeño <juancc0315@gmail.com>
2021-01-13 08:44:50 +0800

    second commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { Product } from '../components/Product'
 import { ProductList } from '../components/ProductList'
 
 
 export const HomePage = () => {
     return (
         <div>
-            {/* <Product/> */}
            <ProductList/>
         </div>
     )
