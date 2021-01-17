b14a7a63c3b2ba04a6398caddc4900f22a7f5354
Juan Cedeño <juancc0315@gmail.com>
2021-01-17 12:58:37 +0800

    Add loading

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../css/loading.css'
+export const Loading = () => {
+    return (
+        <div className = 'loading'>
+
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+        </div>
+    )
+}
